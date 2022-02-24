2596d1c05a3ef7d4d2d9602dc72bf02cf0398eb0
UsefulTornado <Azarel.url@gmail.com>
2022-02-24 23:02:58 +0300

    functions that calculate similarities for users were added

--- metrics.py
+++ metrics.py
@@ -58,3 +58,57 @@ def pearson_similarity(x: np.array, y: np.array) -> float:
         return np.nan
 
     return np.sum(x_b * y_b) / denominator
+
+
+def euclidean_users_similarity(x: np.array, y: np.array) -> float:
+    """Calculates euclidean similarity of two users.
+
+    This function calculates users similarity based on
+    euclidean similarity. The similarity between two users,
+    that represented here as two 1-D arrays where each value
+    is score for some item, is calculated only for
+    those indices i for which there is score both
+    for first and second user. It's assumed that zero values
+    in arrays are equal to lack of score for some items.
+
+    Args:
+        x, y: two 1-D data arrays that represent user's scores.
+
+    Returns:
+        euclidean similarity between two users x and y.
+
+    """
+
+    x_new, y_new = x[(x != 0) & (y != 0)], y[(x != 0) & (y != 0)]
+    
+    if x_new.shape[0] == 0:
+        return -np.inf
+
+    return euclidean_similarity(x_new, y_new)
+
+
+def pearson_users_similarity(x: np.array, y: np.array) -> float:
+    """Calculates pearson similarity of two users.
+
+    This function calculates users similarity based on
+    pearson similarity. The similarity between two users,
+    that represented here as two 1-D arrays where each value
+    is score for some item, is calculated only for
+    those indices i for which there is score both
+    for first and second user. It's assumed that zero values
+    in arrays are equal to lack of score for some items.
+
+    Args:
+        x, y: two 1-D data arrays that represent user's scores.
+
+    Returns:
+        pearson similarity between two users x and y.
+
+    """
+    
+    x_new, y_new = x[(x != 0) & (y != 0)], y[(x != 0) & (y != 0)]
+
+    if x_new.shape[0] == 0:
+        return -np.inf
+    
+    return pearson_similarity(x_new, y_new)
