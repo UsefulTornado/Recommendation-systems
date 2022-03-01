b3ba1e36153935374d1635f5b8169b5ada25000f
UsefulTornado <Azarel.url@gmail.com>
2022-03-01 23:03:42 +0300

    added AP@K and MAP@K metrics

--- metrics.py
+++ metrics.py
@@ -112,3 +112,55 @@ def pearson_users_similarity(x: np.array, y: np.array) -> float:
         return -np.inf
     
     return pearson_similarity(x_new, y_new)
+
+
+def apk(actual: np.array, predicted: np.array, k: int=10) -> float:
+    """Calculates the average precision at k.
+
+    This function calculates the average precision at k
+    for given predicted elements by actual elements.
+
+    Args:
+        actual: array of relevant elements in any order.
+        predicted: array of predicted elements that are sorted
+            by relevance in descending order.
+        k: number of predicted elements.
+
+    Returns:
+        The average precision at k over the input lists.
+
+    """
+    
+    if len(predicted) < k:
+        k = len(predicted)
+
+    precision_at_k = np.array([np.in1d(predicted[:i], actual).sum() / i for i in range(1, k+1)])
+    rel = np.array([1 if p in actual else 0 for p in predicted[:k]])
+
+    return (precision_at_k * rel).mean()
+
+
+def mapk(actual: np.array, predicted: np.array, k: int=10) -> float:
+    """Calculates the mean average precision at k.
+
+    This function calculates the mean average precision at k
+    for given predicted elements for multiple users by
+    actual elements.
+    
+    Args:
+        actual: 2-D array of relevant elements.
+        predicted: 2-D array of predicted elements.
+        k: number of predicted elements.
+
+    Returns:
+        The mean average precision at k over the input arrays.
+
+    """
+    
+    sum = 0
+    U = len(predicted)
+
+    for i in range(U):
+        sum += apk(actual[i], predicted[i], k)
+
+    return sum / U
